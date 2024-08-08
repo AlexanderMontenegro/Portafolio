734ccdc1dcf73e8857663e9df1fd521e4eda89eb
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-08-07 22:42:28 -0300

    actualizacion 07/08-22.42

--- script.js
+++ script.js
@@ -37,4 +37,14 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
   
+
+  document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const nav = document.querySelector('.nav-responsive');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+});
+
   
